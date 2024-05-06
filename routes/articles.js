139b800591f5a612efa1282c1a25c59bf1c3070f
Schnaar <Ashley.Schnaar@btinternet.com>
2024-05-06 14:24:15 +0100

    Fixed new article

--- routes/articles.js
+++ routes/articles.js
@@ -3,7 +3,8 @@ const Article=require('./../models/article')
 const article = require('./../models/article')
 const router = express.Router()
 router.get('/new',(req,res)=>{
-    res.render('articles/new')
+    const article=new Article
+    res.render('articles/new',{article:article})
 })
 router.get('/edit/:id',async (req,res)=>{
     const article= await Article.findById(req.params.id)
